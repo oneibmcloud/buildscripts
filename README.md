7005b7f32a0ff3bff3399944949dad6e2573a04c
Martha DasSarma <dassarma@us.ibm.com>
2015-10-29 15:02:22 -0400

    Fixes#1

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-This project has build and deploy scripts intended to be used in IBM Bluemix DevOps Services Build & Deploy pipelines. 
-In particular, it supports Node JS applications deployed on Bluemix.
+This project has build and deploy scripts intended to be used in the IBM Bluemix DevOps Services Build & Deploy pipelines. 
+
+The blue-green deploy script consists of 10 steps. The following are instructions on how to recover in case of a
+failure from each of the steps. Before you execute any of the 'cf' commands, you need to login onto Bluemix and be in
+the space where the app is being deployed.
+
+1) 
+PROBLEM: Blue-app exists, the script fails. 
+SOLUTION: Delte the blue-app by hand. On the command line, run 'cf delete <blue-app-name>'
+
+2) 
+PROBLEM: 'cf push' of blue app failed.
+SOLUTION: Examine the deploy log to find the cause of the problem, before restarting the
+          deploy, run: 'cf delete <blue-app-name>'
+
+3)
+PROBLEM: 'ct set-env' failed
+SOLUTION:  TBD
+
+4)
+PROBLEM: 'cf start' failed.
+SOLUTION: This indicates a problem either with Bluemix or with the app itself. Check the log for
+details and clean the the blue-app as in problem 1).
+
+5) 
+PROBLEM: Application not responsding.
+SOLUTION: Manually investigate why the application is not responsding. Use the browser to connect to the application
+to see if the problem persists. Clean up and redeploy.
+
+6)
+PROBLEM: Adding additional routes failed.
+SOLUTION: Check the log and investigate why the mapping additional routes failed. 
+
+7)
+PROBLEM: Deleting additional route from blue-app failed
+SOLUTION: Verify that the additional route does not exist, remove it by hand if it does.
+
+8)
+PROBLEM: Deleting backup of green-app failed. 
+SOLUTION: Clean up and restart the deploy.
+
+9)
+PROBLEM: Problems stopping the backup.
+SOLUTION: Clean up and restart the deploy.
+
+10)
+DONE!
--- bin/blue-green-deploy.sh
+++ bin/blue-green-deploy.sh
@@ -2,17 +2,17 @@
 #
 # Description
 # ===========
-# This script does a blue-green deployment of an app to Bluemix. It assumes
-# the user has already logged in. This is done on behalf of the user when running on
-# IBM Bluemix DevOps Services Build & Deploy pipelines. 
+# This script does a blue-green deploy of an app to Bluemix. It assumes
+# the user has already logged in. This script is tailored to be used by 
+# a deploy stage running in an IBM Bluemix DevOps Services Build & Deploy pipeline.
 #
 # The script can do the following things:
-# . Pushes the application
-# . Set environment variables
-# . Map Routes
-# . Bind services
-# . Start the application
-# . Notify a Slack channel upon completion
+# 1. Pushes the application
+# 2. Set environment variables
+# 3. Map Routes
+# 4. Bind services
+# 5. Start the application
+# 6. Notify a Slack channel upon completion or when there is a failure
 #
 # Environment variables
 # =====================
@@ -23,15 +23,21 @@
 #
 # There are other environment variables that need to be set, such as:
 # 1. Slack web hook URL
-# 1. Location of the manifest.yml file
+# 2. Location of the manifest.yml file
+# 3. Directory where to run 'cf push' from, the default is the top level directory
+# 4. Root URL of the app if different from the default
 #****************************************************************
 
 #############
-# Functions 
+# Functions
 #############
 
 notify_slack () {
-   curl -X POST --data-urlencode "payload={ \"attachments\": [{\"title\": \"Job ${BUILD_NUMBER} - Deployment of ${GREEN_APP} failed. \",  \"color\": \"danger\", \"text\": \"<!channel> ${MESSAGE}\"}]}" ${SLACK_WEBHOOK_PATH}
+   MSG=${1-"Visit ${WIKI} for more information."}
+   STEP=${2-unknown}
+   EMOJI=${3-:dash:}
+   COLOR=${4-#ffd400}
+   curl -X POST --data-urlencode "payload={\"icon_emoji\": \"${EMOJI}\",\"username\": \"JOB ${BUILD_NUMBER}\", \"attachments\": [{\"title\": \"Job ${BUILD_NUMBER} - Step ${STEP} \",  \"color\": \"${COLOR}\", \"text\": \"<!channel> ${MSG}\"}]}" ${SLACK_WEBHOOK_PATH}
 }
 
 #############
@@ -41,65 +47,104 @@ notify_slack () {
 BLUE_APP="${CF_APP}-blue"
 GREEN_APP="${CF_APP}-green"
 GREEN_APP_BACKUP="${CF_APP}-green-backup"
-#DOMAIN="oneibmcloud.com"
-DOMAIN="mybluemix.net"
+
+# emoji
+FAILED=":x:"
+PASSED=":white_check_mark:"
+
+# colors
+GREEN=good
+YELLOW=warning
+RED=danger
 
 #############
 # Main Steps
+#
+# Other environment variables, set in the ENVIRONMENT PROPERTIES of 
+# the stage running this script. These are:
+# SLACK_WEBHOOK_PATH
+# MANIFEST_FILE
+# APP_PUSH_DIR
+# URL_PATH_PAGE
+# ROUTES
+# ENVS
+# SERVICES
+# MY_DOMAIN 
+# FAILURE_RECOVERY_WIKI
 #############
+
+# Set defaults
+DOMAIN=${MY_DOMAIN:-"mybluemix.net"}
+PATH_PAGE=${URL_PATH_PAGE:-""}
+PUSH_DIR=${APP_PUSH_DIR:-.}
+WIKI=${FAILURE_RECOVERY_WIKI:-"https://github.com/oneibmcloud/buildscripts"}
+
+# The location of the manifest needs to be set.
+if [ -z ${MANIFEST_FILE} ]; then
+   echo "Environment variable MANIFEST_FILE not set, set it to the path of the manifest file."
+   exit 1
+fi
+
+# default generic message
+ERR_MESSAGE="Visit ${WIKI} for instructions on how to recover from this failure."
+
+CF="cf"
 
 set +e
-
 #############
-# (1) If the blue exits, exit and ask user to clean up before restarting.
+# (1) If the blue app exits, exit and ask user to clean up before restarting.
 #############
-cf app ${BLUE_APP}
-BLUE_APP_EXISTS=$?
-if [ $BLUE_APP_EXISTS -eq 0 ]; then
-   MESSAGE="${BLUE_APP} exits, delete the app manually and restart the deploy. To clean up, on the command line run- cf delete ${BLUE_APP}"
-   notify_slack && exit 1
+$CF app ${BLUE_APP}
+if [ $? -eq 0 ]; then
+   notify_slack "${ERR_MESSAGE}" 1 ${FAILED} ${RED} && exit 1
 fi
 
 #############
-# (2) Push a new version of the app
+# (2) Push a new version of the app.
 #############
-BLUE_APP_PUSH=$?
-if [ $BLUE_APP_PUSH -ne 0 ]; then
-   MESSAGE="Deployment of $BLUE_APP failed"
-   notify_slack && exit 1
+cd ${PUSH_DIR}
+$CF push ${BLUE_APP} -n ${BLUE_APP} -f ${MANIFEST_FILE} --no-start
+if [ $? -ne 0 ]; then
+   notify_slack "${ERR_MESSAGE}" 2 ${FAILED} ${RED} && exit 1
 fi
 
 echo "${BLUE_APP} deployed"
-cf apps | grep ${BLUE_APP}
+$CF apps | grep ${BLUE_APP}
 
 #############
-# (3) Set environment variables
+# (3) Set environment variables.
 #############
-cf set-env ${BLUE_APP} SENDGRID_APIKEY ${SENDGRID_APIKEY}
-
-#############
-# (4) Start blue app
-#############
-cf start ${BLUE_APP}
-START_BLUE_APP=$?
-if [ $START_BLUE_APP -ne 0 ]; then
-   MESSAGE="${BLUE_APP} failed to start, check manually and perform any clean up necessary"
-   notify_slack && exit 1
+if [ -z ${ENVS} ]; then
+   echo "********** Application has no environment variables to set **********"
+else 
+   echo "********** Setting environment variables **********"
+   IFS==',' read -a envvararray <<< "${ENVS}"
+   for element in "${envvararray[@]}"
+      do
+      IFS=':' read -a envvar <<< "$element"
+      $CF set-env ${BLUE_APP} ${envvar[0]} ${envvar[1]}
+   done
 fi
 
 #############
-# (5) Test blue app
+# (4) Start blue app.
 #############
-APP_URL="https://${BLUE_APP}.${DOMAIN}"
+$CF start ${BLUE_APP}
+if [ $? -ne 0 ]; then
+   notify_slack "${ERR_MESSAGE}" 4 ${FAILED} ${RED} && exit 1
+fi
+
+#############
+# (5) Test blue app.
+#############
+APP_URL="https://${BLUE_APP}.${DOMAIN}/${PATH_PAGE}"
 for i in {1..3}; do
     CURL_EXIT=`curl "$APP_URL" -s -k -w "%{http_code}" -X POST -H 'Content-Type: application/json' -d "$JSON" -o /dev/null`
     if [ '200' == "$CURL_EXIT" ]; then
         echo "${APP_URL} notified successfully."
         break
     elif [ $i == 3 ]; then
-        MESSAGE="${APP_URL} notification failed. Code $CURL_EXIT."
-        notify_slack 
-        exit 1
+        notify_slack "${ERR_MESSAGE}i" 5 ${FAILED} ${RED} && exit 1
     else
         echo "${APP_URL} notification failed. Code $CURL_EXIT. Retrying..."
         sleep 2
@@ -107,64 +152,93 @@ for i in {1..3}; do
 done
 
 #############
-# (6) Map traffic to the new version of the app by binding to public host
-# Note: The old version is still taking traffic to avoid disruption in 
-# service
+# (6) Route traffic to the new instance of the app by binding to public host.
+# Note: The old version is still taking traffic to avoid disruption in service.
 #############
-cf map-route ${BLUE_APP} ${DOMAIN} -n ${CF_APP}
-MAPROUTE_BLUE=$?
-if [ $MAPROUTE_BLUE -ne 0 ]; then 
-   MESSAGE="Failed to create the public route ${CF_APP}."
-   notify_slack && exit 1
+$CF map-route ${BLUE_APP} ${DOMAIN} -n ${CF_APP}
+if [ $? -ne 0 ]; then
+   notify_slack "${ERR_MESSAGE}" 6 ${FAILED} ${RED} && exit 1
+fi
+
+# Map additional routes
+if [ -z ${ROUTES} ]; then
+   echo "*********** No additional routes to map ***********"
+else 
+   echo "Mapping routes in order to balance traffic"
+   IFS=',' read -a routesarray <<< "$ROUTES"
+   for element in "${routesarrary[@]}"
+   do
+      if [[ $element == *":"* ]]; then
+         IFS=':' read -a route <<< "$element"
+         $CF map-route ${BLUE_APP} ${route[1]} -n ${route[0]}
+      else
+         $CF map-route ${BLUE_APP} $element
+      fi
+   done
 fi
 
 echo "Public route bindings"
-cf routes | grep ' $CF_APP '
-
+$CF routes | grep ' $CF_APP '
 #############
-# (7) Delete the temporary route that was used for testing since
-# it is not longer needed.
+# (7) Delete the temporary route that was used for testing, it is not longer needed.
 #############
-cf unmap-route ${BLUE_APP} $DOMAIN -n ${BLUE_APP}
+$CF unmap-route ${BLUE_APP} $DOMAIN -n ${BLUE_APP}
 if [ $? -ne 0 ]; then
    echo "Test route isn't mapped and didn't have to be removed"
 fi
-cf delete-route $DOMAIN -n ${BLUE_APP} -f
+$CF delete-route $DOMAIN -n ${BLUE_APP} -f
 
 #############
-# 8) rename green green-backup
+# 8) Rename green to green-backup, thus preserving previous version.
 #############
-cf app ${GREEN_APP_BACKUP}
+$CF app ${GREEN_APP_BACKUP}
 if [ $? -eq 0 ]; then
    echo "${GREEN_APP_BACKUP} exists, removing it before creating a new backup"
-   cf delete ${GREEN_APP_BACKUP} -f
+   $CF delete ${GREEN_APP_BACKUP} -f
    if [ $? -ne 0 ]; then
-      MESSAGE="Removing ${GREEN_APP_BACKUP} failed, manually recover from this failure."
-      notify_slack && exit 1
+      notify_slack "${ERR_MESSAGE}" 8 ${FAILED} ${RED} && exit 1
    fi
 fi
 
-cf rename ${GREEN_APP} ${GREEN_APP_BACKUP}
+$CF rename ${GREEN_APP} ${GREEN_APP_BACKUP}
 if [ $? -ne 0 ]; then
-   MESSAGE="Creating backup of previously deployed app failed, manually recover from this failure."
-   notify_slack && exit 1
+   notify_slack "${ERR_MESSAGE}" 8 ${FAILED} ${RED}&& exit 1
 fi
 
 #############
 # 9) Rename blue -> green
 #############
-cf rename ${BLUE_APP} ${GREEN_APP}
+$CF rename ${BLUE_APP} ${GREEN_APP}
 if [ $? -ne 0 ]; then
-   MESSAGE="Failed to rename ${BLUE_APP} to ${GREEN_APP}, manually recover from this failure."
-   notify_slack && exit 1
+   notify_slack "${ERR_MESSAGE}" 9 ${FAILED} ${RED} && exit 1
 fi
 
 #############
 # 10) Stop backup app
 #############
-cf stop ${GREEN_APP_BACKUP}
+$CF map-route ${GREEN_APP_BACKUP} ${DOMAIN} -n ${GREEN_APP_BACKUP}
 if [ $? -ne 0 ]; then
-   MESSAGE="Manually stop ${GREEN_APP_BACKUP}"
-   notify_slack && exit 1
+   notify_slack "${ERR_MESSAGE}" 10.1 ${FAILED} ${RED} && exit 1
 fi
+$CF unmap-route ${GREEN_APP_BACKUP} ${DOMAIN} -n ${CF_APP}
+if [ $? -ne 0 ]; then
+   notify_slack "${ERR_MESSAGE}" 10.2 ${FAILED} ${RED} && exit 1
+fi
+for i in {1..3}; do
+   $CF stop ${GREEN_APP_BACKUP}
+   if [ $? -eq 0 ]; then
+      echo "${GREEN_APP_BACKUP} was stopped successfully, done!"
+      break
+   elif [ $i -eq 3 ]; then
+      notify_slack "${ERR_MESSAGE}" 10.3 ${FAILED} ${RED} && exit 1
+   else
+      echo "Retrying $i 'cf stop'..."
+      sleep 2
+   fi
+done
+
+#############
+# 11) Post a successful message to Slack
+#############
+notify_slack "${GREEN_APP} deployed successfully!!!" 11 ${PASSED} ${GREEN} 
 set -e
